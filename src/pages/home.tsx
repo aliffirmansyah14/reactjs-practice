b466c91f82b42669b738e3d46ebd9ee8aaa303ba
aliffirmansyah14 <aliffirmansyah1407@gmail.com>
2025-09-13 16:14:18 +0700

    add:shadcn ui

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 const Home = () => {
+	const [count, setCount] = useState(0);
 	return (
 		<div className="max-w-md mx-auto py-10">
 			<h1 className="font-semibold">Hello world !</h1>
+			<Button onClick={() => setCount(count + 1)}>{count} likes</Button>
 		</div>
 	);
 };
